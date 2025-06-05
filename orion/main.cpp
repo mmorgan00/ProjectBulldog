d38ec338de8ac438c17913266aa5e22409fe9a64
Max Morgan <maxwellmorgan23@gmail.com>
2025-06-05 11:52:58 -0600

    feat: Added initial JSON config file support
    As it turns out, fastgltf ships with simdjson included. No new
    dependencies are being introduced and yet there is now json config file
    support

--- orion/main.cpp
+++ orion/main.cpp
@@ -1,11 +1,28 @@
+
+#include "simdjson.h"
+
 #include "entry.hpp"
 #include "core/renderer.hpp"
+#include "util/logger.hpp"
+
+using namespace simdjson;
 
 int main(int argc, char* argv[]) {
+  DECLARE_LOG_CATEGORY(ORION);
+  // Load config
+  ondemand::parser parser;
+  padded_string json = padded_string::load("../../config/engine.conf");
+  ondemand::document config = parser.iterate(json);
+  OE_LOG(ORION, INFO, "{}", std::string(config["appName"]));
+  OE_LOG(ORION, INFO, "Running using {}", std::string(config["graphicsAPI"]));
+  // Init modules
   Renderer renderer;
   renderer.init();
+  // Run game
   init();
   run();
+  // Cleanup process
+  // TODO: Expose a 'quit game' api
   renderer.shutdown();
   return 0;
 }
